25bf19fe97e4d225ae3b416949b5039c65c2a811
HeltonNoN <151919896+HeltonNoN@users.noreply.github.com>
2024-03-15 13:21:39 +0300

    Update to8f8y.py

--- to8f8y.py
+++ to8f8y.py
@@ -1,1 +1,13 @@
+def f(arr):
+    if len(arr)<=1:
+        return arr
+    else:
+        bzn=arr[0]
+        left=[x for x in arr[1:] if x<bzn]
+        right=[x for x in arr[1:] if x>=bzn]
+        return f(left)+[bzn]+f(right)
+arr=[3,1,4,2,4]
+print(f(arr))
 
+import random
+random.choise(string)
